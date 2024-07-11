30443d72cc10b9f3b87781846b612e9ba99c96e4
Lexari0 <lexario.makes@gmail.com>
2024-07-10 17:32:03 -0700

    Added missing  for call to

--- libs/gallery.js
+++ libs/gallery.js
@@ -369,7 +369,7 @@ ${required_query.replace(/^/gm,"    ")}
     getItemIDOfFile: async function(file_path) {
         await db.select("gallery_item_id", "items", {
             distinct: true,
-            where: `hash=${this.hashFile(file_path)}`
+            where: `hash=${await this.hashFile(file_path)}`
         });
     },
     refreshContent: async function() {
