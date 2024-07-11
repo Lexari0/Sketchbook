0b5d7f3a1048523b58ee01e7b308d4cff8b8438a
Lexari0 <lexario.makes@gmail.com>
2024-07-10 17:32:04 -0700

    Added config api endpoints

---
+++ endpoints/api/config.js
@@ -0,0 +1,62 @@
+const path = require("path");
+const api = require(path.join(process.cwd(), "libs/api.js"));
+const config = require(path.join(process.cwd(), "libs/config.js"));
+
+module.exports = {
+    register_endpoints: endpoints => {
+        endpoints["/api/config"] = async (req, res) => {
+            if (!await api.requestIsValid(req, res, config.api.enabled_endpoints.config._)) {
+                return true;
+            }
+            api.sendResponse(res, 200, {error:"", config})
+            return true;
+        };
+        endpoints["/api/config/update"] = async (req, res) => {
+            if (!await api.requestIsValid(req, res, config.api.enabled_endpoints.config.update)) {
+                return true;
+            }
+            const params = await api.getParams(req);
+            var new_config = config.clone();
+            var errors = [];
+            for (const key of Object.keys(params))
+            {
+                var key_layers = key.split(".");
+                const last_key_layer = key_layers.pop();
+                var config_obj = new_config;
+                for (const key_layer of key_layers)
+                {
+                    config_obj = config_obj[key_layer];
+                    if (typeof(config_obj) !== "object")
+                    {
+                        config_obj = undefined;
+                        break;
+                    }
+                }
+                if (config_obj == undefined || config_obj[last_key_layer] == undefined)
+                {
+                    errors.push(`Key referenced invalid path: ${key}`);
+                }
+                else if (typeof(config_obj[last_key_layer]) == "object")
+                {
+                    errors.push(`Key referenced an object, which cannot be set via the API: ${key}`);
+                }
+                if (errors.length == 0)
+                {
+                    config_obj[last_key_layer] = params[key];
+                }
+            }
+            if (errors.length > 0)
+            {
+                api.sendResponse(res, 200, {error: errors.join("; "), params})
+                return true;
+            }
+            for (const k of Object.keys(params).map(key => key.substring(0, key.indexOf(" "))))
+            {
+                config[k] = structuredClone(new_config[k]);
+            }
+            config = {...config, ...new_config};
+            api.sendResponse(res, 200, {error: "", params})
+            return true;
+        };
+    }
+};
